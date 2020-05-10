ca3f6276d2bd529c3f46dd9aa0b34f5caaadbe70
Horia <ionescho@gmail.com>
2020-05-10 21:08:41 +0300

    change location of Layout and Auxiliary

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,20 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Modal.css';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = (props) => {
-    return <React.Fragment>
-        <Backdrop clicked={props.modalClosed} show={props.show}/>
-        <div 
+class Modal extends Component {
+
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.show !== this.props.show;
+    }
+
+    render() {
+        return <React.Fragment>
+            <Backdrop clicked={this.props.modalClosed} show={this.props.show}/>
+            <div 
                 className={classes.Modal}
                 style={{
-                    transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                    opacity: props.show ? "1" : "0"
+                    transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                    opacity: this.props.show ? "1" : "0"
                 }}
-                >
-            {props.children}
-        </div>
-    </React.Fragment>;
+            >
+                {this.props.children}
+            </div>
+        </React.Fragment>;
+    }
 }
 
-export default modal;
+export default Modal;
